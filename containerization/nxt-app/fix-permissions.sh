5272ad765e1c93474fbe7f9dd81d2afa2329851f
Ubuntu <robi@dockerize.damn.my.id>
2025-12-03 21:37:58 +0700

    Upgrade Step

--- containerization/nxt-app/fix-permissions.sh
+++ containerization/nxt-app/fix-permissions.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 chown -R www-data:www-data /var/www/html/data
 chown -R www-data:www-data /var/www/html/config
+
+if [ -f /var/www/html/occ ]; then
+    echo "Running automatic Nextcloud upgrade (if needed)"
+    su -s /bin/sh www-data -c "php /var/www/html/occ upgrade" || true
+fi
+
 exec "$@"
